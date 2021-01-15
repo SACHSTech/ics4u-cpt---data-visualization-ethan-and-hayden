2acc1c843958f639682cad1e6adbd12432d58d9f
EthanLai8 <Ethan.lai21@ycdsbk12.ca>
2021-01-14 22:05:58 -0500

    Fixed .csv folder and added Games.java

---
+++ src/basic/Games.java
@@ -0,0 +1,8 @@
+package basic;
+
+public class Games {
+    public Games(String strTitle, boolean isHandheld, int intMaxPlayers, boolean isMultiplayer, String strGenre, String strLicensed, boolean isSequel, int intScore, double dblSales, double dblPrice, String strConsole, String strRating, boolean isRereleased, int intYear, double dblPlaytime) {
+        
+    }
+    
+}
